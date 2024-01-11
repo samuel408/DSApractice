16355c27d3988377b5bfa9856b3896c25550a42e
Samuel408 <samuelsaldivar11@gmail.com>
2024-01-11 00:47:19 -0800

    sorted using orderedDict python library.

--- Arrays_and_Hashing/Top_k_elements.py
+++ Arrays_and_Hashing/Top_k_elements.py
@@ -12,18 +12,21 @@ class Solution(object):
                 regmap[i] += 1
             else:
                 regmap[i] = 1
-       # sort dictionary by its key value in decensing order
-        sorted_dict = dict(sorted(regmap.items(), key=lambda x: x[1], reverse=True))
+    #    sort dictionary by its key value in decensing order
+        sorted_dict = OrderedDict(sorted(regmap.items(), key=lambda x: (x[1], x[0]), reverse=True))
 
         # grab the top K elements and put them in the solution array
         # create solution array 
         solution = []
+        # top = 0
         for key in sorted_dict.keys():
             if len(solution) == k:
                 break
             solution.append(key)
             i += 1
-
+        # for key,value in regmap.items():
+        #     if value > top:
+        #         solution[0] = value 
         print(regmap)
         print(sorted_dict)
         return solution
